b79994dc0af4afea7e111d4564552f07bbf072e9
Gusakov Egor <gusakovegor1@gmail.com>
2024-07-23 19:14:14 +0300

    Update config cs-fixer

--- tests/Eegusakov/GeoSearch/Loggers/ConsoleLoggerTest.php
+++ tests/Eegusakov/GeoSearch/Loggers/ConsoleLoggerTest.php
@@ -20,6 +20,18 @@ final class ConsoleLoggerTest extends TestCase
         $this->logger = new ConsoleLogger();
     }
 
+    public static function providePrintMessageCases(): iterable
+    {
+        yield ['emergency', "\e[31m[EMERGENCY] emergency" . PHP_EOL];
+        yield ['alert', "\e[34m[ALERT] alert" . PHP_EOL];
+        yield ['critical', "\e[31m[CRITICAL] critical" . PHP_EOL];
+        yield ['error', "\e[31m[ERROR] error" . PHP_EOL];
+        yield ['warning', "\e[33m[WARNING] warning" . PHP_EOL];
+        yield ['notice', "\e[33m[NOTICE] notice" . PHP_EOL];
+        yield ['info', "\e[39m[INFO] info" . PHP_EOL];
+        yield ['debug', "\e[39m[DEBUG] debug" . PHP_EOL];
+    }
+
     /**
      * @dataProvider providePrintMessageCases
      */
@@ -35,17 +47,5 @@ final class ConsoleLoggerTest extends TestCase
         $this->logger->log(3, 'level three');
 
         $this->expectOutputString("\e[39m[LOG][3] level three" . PHP_EOL);
-    }
-
-    public static function providePrintMessageCases(): iterable
-    {
-        yield ['emergency', "\e[31m[EMERGENCY] emergency" . PHP_EOL];
-        yield ['alert', "\e[34m[ALERT] alert" . PHP_EOL];
-        yield ['critical', "\e[31m[CRITICAL] critical" . PHP_EOL];
-        yield ['error', "\e[31m[ERROR] error" . PHP_EOL];
-        yield ['warning', "\e[33m[WARNING] warning" . PHP_EOL];
-        yield ['notice', "\e[33m[NOTICE] notice" . PHP_EOL];
-        yield ['info', "\e[39m[INFO] info" . PHP_EOL];
-        yield ['debug', "\e[39m[DEBUG] debug" . PHP_EOL];
     }
 }
